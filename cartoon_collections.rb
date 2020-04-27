f1613a55419f7bb5b834ae604e7910ba0fbca823
dapper-module-0474 <rjwhitmercc@gmail.com>
2020-04-27 19:13:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,8 @@ def greet_characters(array)
   # Print a custom greeting for each element
   
   array.each do |element|
-    puts "Hello #{element}!
-  end"
+    puts "Hello #{element}!"
+  end
 end
 
 def list_dwarves(array)
