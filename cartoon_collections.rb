c22f3e70fbe0885502fe4cc173ffe503c76501a9
dapper-module-0474 <rjwhitmercc@gmail.com>
2020-04-27 19:16:26 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,10 +2,20 @@ def greet_characters(array)
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
+  
+  array.each do |element|
+    puts "Hello #{element}!"
+  end
 end
 
 def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
+  dwarf_number = 1
+  
+  array.each do |element|
+    print dwarf_number, ". #{element}"
+    dwarf_number += 1 
+  end
 end
